d64a29feae5dd0d2d52e2f3c28d5791ecc3d0d6a
Derek Tamsen <derek@shotgunsoftware.com>
2011-10-18 11:55:28 -0700

    - Changed interpreter to use "env ruby" instead of ruby - Added an encoding line for utf-8 - Added logging level for verbosity level - Added a base script path - Using the base script path to build some of the other dir structures. - Script now calls sync-process.sh which performs an rsync of the directories. - Fixed an issue where the script would go into an infinite loop when the number of directories to process was less than the max parallel procs.
    ISSUES:
    - Script does not handle an rsh ssh rsync due to quoting issues.

--- scheduled_rsync_parallel/ha-sync-parallel.rb
+++ scheduled_rsync_parallel/ha-sync-parallel.rb
@@ -1,33 +1,75 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
+
+# encoding: UTF-8
 
 # Variables
-$basesourcedir = "/tmp/test"          # Base Directory to be backed up
+$basesourcedir = "/tmp/test"    # Base Directory to be backed up
 
-$maxparallelproc = 8                     # Number of parallel rsync processes to run
-$sleeptime = 0.8                        # time to sleep while checking pids.
-                                        # can be increased for longer running processes
-                                        # to reduce parent proc cpu usage
+$basescriptloc = "/home/user/scheduled_rsync_parallel" # Location of script
+
+$maxparallelproc = 7    # Number of parallel rsync processes to run
+$sleeptime = 1  # time to sleep while checking pids.
+                # can be increased for longer running processes
+                # to reduce parent proc cpu usage
 
 # rsync related information
 # rsync program location
-$rsync = "/usr/bin/rsync"                  # file location of rsync
+$rsyncscript = "#{$basescriptloc}/sync-process.sh"
+$rsyncbin = "/usr/bin/rsync"    # file location of rsync
 
 # rsync options
-$rproto = "ssh -i /root/.ssh/rsync-key"    # rsync proto options
-$flags = "-av -e"                          # rsync flags
+#$rproto = "ssh -i /root/.ssh/rsync-key"    # rsync proto options ex. with ssh key need to also set -e under rflags
+#$rproto = "ssh -l dtamsen -p 22"
+#$rflags = "-av -e" # rsync flags ex. specifying the rsh command
+$rflags = "-avvv" # rsync flags
+
 
 # source and destinations
-$source = "/tmp/file_to_send"              # files to send
-$dest = "server2:/tmp/"                    # directory to store files
+$source = $basesourcedir    # files to send
+$dest = "/tmp/test dest/"   # directory to store files
+
+# Log config
+# log location
+$logdir = "#{$basescriptloc}/log"
+$loglevel =  "debug"    # supports [debug,quiet]
 
 # Location to store lock files
-#$lockdir = "/var/lock/ha-sync-parallel"            # location for lock file
-$lockdir = "/Users/dtamsen/git_working/derektamsen/cron_scripts/scheduled_rsync_parallel/lock"
+#$lockdir = "/var/lock/ha-sync-parallel"    # location for lock file
+$lockdir = "#{$basescriptloc}/lock"
 $lockfileparent = "ha-sync-parallel-parent"
 
 
+#### Requires
+
 
 #### Begin Functions
+# Set logging level to number
+def loglevel(level)
+    case level
+        when "debug"
+            puts "logging level: " + $loglevel.to_s
+            $loglevel = 1
+        else
+            $loglevel = 0
+    end # case level
+end # loglevel func
+
+# Create directories
+def createdir(dir)
+    if File.directory?(dir) == false
+        puts "directory: " + dir
+        puts "does not exist. Creating..."
+        begin
+            # Create lock directory with u+rwx go+rx
+            Dir.mkdir(dir, 0755)
+        rescue
+            raise "unable to create dir: " + dir
+            exit
+        end # end mkdir rescue
+        puts "directory created"
+    end # end lockdir check
+end # end create dir function
+
 # Get the directory listing based on basesourcedir and split it for parallel processes
 def builddir()
     if File.directory?($basesourcedir) == true
@@ -45,6 +87,12 @@ def builddir()
         raise $basesourcedir + " Is missing or not a directory."
     end
 end # end builddir
+
+# On roadmap
+# Logging function
+#def log()
+#    require 'logger'
+#end # end log function
 
 def createsubproclock(dir, pid)
     puts "Creating lock file for " + dir
@@ -95,19 +143,44 @@ def syncproc(dir)
         # For now DO NOT DIRECTLY PASS TO KILL OR IT WILL KILL THE ABSOULTE VALUE
         return -99999
     else
-    num = 5 + rand(30)
-    proc = IO.popen("sleep " + num.to_s)
+    #num = 5 + rand(30)
+    #proc = IO.popen('echo start; sleep ' + num.to_s + ' | tee -a ' + $logdir + '/' + dir + '_sync.log; echo stop')
+    # Check to see if rproto has anything specified
+    if $rproto == nil
+        puts "running rsync without rproto options" if $loglevel == 1
+        
+        # Need to fix human readable options. Having issues with spaces and other characters.
+        #puts "#{$rsyncscript} --rsyncbin='#{$rsyncbin}' --rflags='#{$rflags}' --source='#{$source}/#{dir}' --dest='#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'" if $loglevel == 1
+        #proc = IO.popen("#{$rsyncscript} --rsyncbin='#{$rsyncbin}' --rflags='#{$rflags}' --source='#{$source}/#{dir}' --dest='#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'")
+        
+        puts "#{$rsyncscript} '#{$rsyncbin}' '#{$rflags}' '#{$source}/#{dir}' '#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'" if $loglevel == 1
+        proc = IO.popen("#{$rsyncscript} '#{$rsyncbin}' '#{$rflags}' '#{$source}/#{dir}' '#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'")
+        
+    elsif $rproto.length > 0
+        puts "running rsync with rproto options" if $loglevel == 1
+        puts "remote rsync not currently working"
+        
+        # Need to fix human readable options. Having issues with spaces and other characters.
+        # puts "#{$rsyncscript} --rsyncbin='#{$rsyncbin}' --rflags='#{$rflags}' --rproto='#{$rproto}' --source='#{$source}/#{dir}' --dest='#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'" if $loglevel == 1
+        #         proc = IO.popen("#{$rsyncscript} --rsyncbin='#{$rsyncbin}' --rflags='#{$rflags}' --rproto='#{$rproto}' --source='#{$source}/#{dir}' --dest='#{$dest}' | tee -a '#{$logdir}/#{dir}_sync.log'")
+        
+        # puts "#{$rsyncscript} '#{$rsyncbin}' '#{$rflags}' '#{$source}/#{dir}' '#{$dest}' '#{$rproto}' | tee -a '#{$logdir}/#{dir}_sync.log'" if $loglevel == 1
+        # proc = IO.popen("#{$rsyncscript} '#{$rsyncbin}' '#{$rflags}' '#{$source}/#{dir}' '#{$dest}' '#{$rproto}' | tee -a '#{$logdir}/#{dir}_sync.log'")
+        
+    else
+        raise "Error with the rproto variable. If you are not using it please comment it out"
+    end # rproto in use
     createsubproclock(dir, proc.pid)
     Process.detach(proc.pid)
     return proc.pid
-end # end checksubproclock
+    end # end checksubproclock
 end # end syncproc
 
 def synccheckdel(pid, dir)
     begin
         # check to see if we get the pid back. If we do that means it is still running
         Process.getpgid(pid)
-        #puts "still running: " + pid.to_s
+        puts "still running: " + pid.to_s if $loglevel == 1
     # If the process is not running we will get a return of Errno::ESRCH
     rescue Errno::ESRCH
         # remove non active pid so we can launch another in its place
@@ -129,19 +202,13 @@ end # end synccheckdel
 
 
 #### Main Program
-# Check to see if the lock directory exists
-if File.directory?($lockdir) == false
-    puts "Lock dir: " + $lockdir
-    puts "does not exist. Creating..."
-    begin
-        # Create lock directory with u+rwx go+rx
-        Dir.mkdir($lockdir, 0755)
-    rescue
-        raise "unable to create lock dir"
-        exit
-    end # end mkdir rescue
-    puts "lock dir created"
-end # end lockdir check
+# Set logging level
+loglevel($loglevel)
+
+# Create the lock directory if it does not exist
+createdir($lockdir)
+# Create the log directory if it does not exist
+createdir($logdir)
 
 # Ensure we are not currently running
 if File.exists?($lockdir + "/" + $lockfileparent) == true
@@ -164,11 +231,16 @@ end # end currently running check
 $sourcedir = builddir
 
 $pids = {}
+$maxparallelprocsync = $maxparallelproc
+$maxparallelprocclean = $maxparallelproc
 until $sourcedir.empty?
 # Launch sub processes until parallelisim = $maxparallelproc
-while $pids.length < $maxparallelproc
+until $pids.length > $maxparallelprocsync or $sourcedir.empty?
+    puts "pids.len " + $pids.length.to_s if $loglevel == 1
+    puts "pids " + $pids.to_s if $loglevel == 1
     # Remove dir from array and prep for copy
     dirtocopy = $sourcedir.pop
+    puts "sourcedir: " + $sourcedir.to_s if $loglevel == 1
     # check to see if we have done all the work to prevent errors
     unless dirtocopy == nil
         # call the copy function and store the pid
@@ -179,23 +251,31 @@ while $pids.length < $maxparallelproc
             puts "copying " + $basesourcedir + "/" + dirtocopy
         end # end skip if sub dir locked
     end # end work done check
+    
+    sleep $sleeptime
+    puts "pids.length "+ $pids.length.to_s if $loglevel == 1
+    puts "sourcedir.len " + $sourcedir.length.to_s if $loglevel == 1
 end # end maxprallelproc
 
 puts "Left to copy " + $sourcedir.length.to_s
+#sleep $sleeptime
 
 # loop until # processes is greater than maxparallelproc
-while $pids.length >= $maxparallelproc
+while $pids.length != 0 or $pids.length >= $maxparallelprocclean
     # Check each pid to see if it is currently running
     $pids.each_value {|pid| synccheckdel(pid, $pids.index(pid))}
     # Sleep some so we don't consume a lot of cpu
     sleep $sleeptime
+    puts "cleaning loop: " + $pids.length.to_s if $loglevel == 1
 end # end while $pids.length >= maxparallelproc
 end # end until sourcedir is empty
 
 # wait until all processes have exited before stopping script
-Process.waitall
+puts "Waiting for remaining processes to exit"
+$pids.each_value {|pid| Process.wait(pid)}
 
 # cleanup the any remaining processes
+puts "Cleaning up remaining processes"
 $pids.each_value {|pid| synccheckdel(pid, $pids.index(pid))}
 
 # run some cleanup tasks
